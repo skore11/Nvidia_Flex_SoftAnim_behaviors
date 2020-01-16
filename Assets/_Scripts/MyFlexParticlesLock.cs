1f96ad066625c740dc4674cb7b1e8b81c9360f85
skore11 <abishek110790@gmail.com>
2020-01-16 16:17:32 -0500

    Revert uflex, part 3, move changes to flexparticleslock into our own subclass.

---
+++ Assets/_Scripts/MyFlexParticlesLock.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using uFlex;
+
+public class MyFlexParticlesLock : FlexParticlesLock
+{
+    private FlexContainer m_cntr;
+    private TriggerParent m_triggerParent;
+
+    public void Awake()
+    {
+        m_triggerParent = FindObjectOfType<TriggerParent>();
+    }
+
+    public override void PostContainerUpdate(FlexSolver solver, FlexContainer cntr, FlexParameters parameters)
+    {
+        m_cntr = cntr;  // cache local reference for Gizmo drawing
+        if (m_triggerParent == null)
+        {
+            // have to potentially look for TriggerParent again, since it's added dynamically
+            // TODO: rethink this logic, there are many TriggerParents, but this only looks for one!
+            m_triggerParent = FindObjectOfType<TriggerParent>();
+        }
+        if (m_triggerParent.changeCollider)
+        {
+            for (int i = 0; i < cntr.m_particlesCount; i++)
+            {
+                Collider collider = GetComponent<Collider>();
+                Collider[] colliders = Physics.OverlapSphere(cntr.m_particles[i].pos, 1.0f);
+                foreach (Collider c in colliders)
+                {
+                    if (c == collider)
+                    {
+                        m_lockedParticlesIds.Add(i);
+                        m_lockedParticlesMasses.Add(cntr.m_particles[i].invMass);
+                        cntr.m_particles[i].invMass = 0.0f;
+                    }
+                }
+            }
+            m_triggerParent.changeCollider = false;
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (m_cntr != null)
+        {
+            Gizmos.color = Color.red;
+            for (int i = 0; i < m_lockedParticlesIds.Count; i++)
+            {
+                Gizmos.DrawSphere(m_cntr.m_particles[m_lockedParticlesIds[i]].pos, radius: 0.2f);
+            }
+        }
+    }
+}
--- Assets/uFlex/Scripts/Processors/FlexParticlesLock.cs
+++ Assets/uFlex/Scripts/Processors/FlexParticlesLock.cs
@@ -12,13 +12,7 @@ namespace uFlex
         public bool m_lock = false;
         public List<int> m_lockedParticlesIds = new List<int>();
         public List<float> m_lockedParticlesMasses = new List<float>();
-        public FlexContainer m_cntr;
-        private bool changeCol;
 
-        //private void Start()
-        //{
-        //    changeCol = FindObjectOfType<TriggerParent>().changeCollider;
-        //}
 
 
         public override void FlexStart(FlexSolver solver, FlexContainer cntr, FlexParameters parameters)
@@ -37,34 +31,11 @@ namespace uFlex
                     }
                 }
             }
-            
         }
 
 
         public override void PostContainerUpdate(FlexSolver solver, FlexContainer cntr, FlexParameters parameters)
         {
-            changeCol = FindObjectOfType<TriggerParent>().changeCollider;
-            if (changeCol == true)
-            {
-                print(changeCol);
-                for (int i = 0; i < cntr.m_particlesCount; i++)
-                {
-                    Collider collider = GetComponent<Collider>();
-                    Collider[] colliders = Physics.OverlapSphere(cntr.m_particles[i].pos, 1.0f);
-                    foreach (Collider c in colliders)
-                    {
-                        if (c == collider)
-                        {
-                            m_lockedParticlesIds.Add(i);
-                            m_lockedParticlesMasses.Add(cntr.m_particles[i].invMass);
-                            cntr.m_particles[i].invMass = 0.0f;
-                        }
-                    }
-                }
-                changeCol = false;
-                FindObjectOfType<TriggerParent>().changeCollider = false;
-            }
-
             for (int i = 0; i < m_lockedParticlesIds.Count; i++)
             {
                 if (m_lock)
@@ -72,17 +43,15 @@ namespace uFlex
                 else
                     cntr.m_particles[m_lockedParticlesIds[i]].invMass = m_lockedParticlesMasses[i];
             }
-            
         }
 
         void OnDrawGizmosSelected()
         {
-
-            Gizmos.color = Color.red;
-            for (int i = 0; i < m_lockedParticlesIds.Count; i++)
-            {
-                Gizmos.DrawSphere(m_cntr.m_particles[m_lockedParticlesIds[i]].pos, radius: 0.2f);
-            }
+            //Gizmos.color = Color.red;
+            //for (int i = 0; i < m_lockedParticlesIds.Count; i++)
+            //{
+            //    Gizmos.DrawSphere(m_cntr.m_particles[m_lockedParticlesIds[i]].pos, 0.2f);
+            //}
         }
     }
 }
